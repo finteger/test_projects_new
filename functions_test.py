f67e6d1afc316b217679e55b83db8fc1b9708df7
finteger <nashassociatesinc@gmail.com>
2026-05-11 18:58:23 -0600

    update

---
+++ functions_test.py
@@ -0,0 +1,13 @@
+import pytest
+from functions import add, minus
+
+#test case for add function
+def test_add():
+    result = add(2 , 3)
+    assert result == 5
+    
+def test_minus():
+    result = minus(5, 2)
+    assert result == 3
+    
+    
