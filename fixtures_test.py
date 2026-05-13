e4712bdae0fca7a17a78f7a7c45fd4452a043999
finteger <nashassociatesinc@gmail.com>
2026-05-12 18:57:42 -0600

    Update

---
+++ fixtures_test.py
@@ -0,0 +1,11 @@
+from fixtures import simple_list, simple_dict
+
+
+#asserting item in list
+def test_simple_list(simple_list):
+    assert 3 in simple_list
+    
+#asserting values to keys in dictionary   
+def test_simple_dict(simple_dict):
+    assert simple_dict["name"] == "Bryan"
+    assert simple_dict["age"] == 30
